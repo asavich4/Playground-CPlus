95dc528721983b0e1af8b2f5eff3bcabcdb096b1
Alexandar Savich <112600558+asavich4@users.noreply.github.com>
2023-09-14 19:32:51 -0400

    Funtion

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
+void function(){
+    cout << "play";
+}
+
 int main() {
     int test[] = {1,2,3,5,6, 7, 4,3,3,5};
     int size = sizeof(test) / sizeof(test[0]);
 
     for(int i; i < size; i++){
-        cout << test[i];
+        function();
+        cout << " " << test[i] << " ";
     }
 
     cout << " the size is " << size;
