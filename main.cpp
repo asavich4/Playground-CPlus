3425f2788dcaf038ebfb4c8578381d8b4150b0c6
Alexandar Savich <112600558+asavich4@users.noreply.github.com>
2023-09-14 19:25:17 -0400

    ForLoop Array

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main() {
     int test[] = {1,2,3,5,6, 7, 4,3,3,5};
     int size = sizeof(test) / sizeof(test[0]);
 
-    cout << size;
+    for(int i; i < size; i++){
+        cout << test[i];
+    }
+
+    cout << " the size is " << size;
     return 0;
 }
